b1d4afbb0f08ae9032b85c8b29cee8e0dc71d0e8
Mi Gao <398938578@qq.com>
2021-06-25 21:26:04 +0800

    init status

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,32 +1,51 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import React, { useCallback, useEffect } from 'react'
+import { View, Text, Image, Button } from '@tarojs/components'
+import {
+  useReady,
+  useDidShow,
+  useDidHide,
+  usePullDownRefresh
+} from '@tarojs/taro'
 import { AtButton } from 'taro-ui'
-
-import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './index.css'
 
-export default class Index extends Component {
+function Index() {
 
-  componentWillMount () { }
 
-  componentDidMount () { }
+  const avatar = require('../../utils/avatar');
+  const handleClick = useCallback(()=>{
+    console.log("bingo")
+  },[])
 
-  componentWillUnmount () { }
+  // 可以使用所有的 React Hooks
+  useEffect(() => { })
 
-  componentDidShow () { }
+  // 对应 onReady
+  useReady(() => { })
 
-  componentDidHide () { }
+  // 对应 onShow
+  useDidShow(() => { })
 
-  render () {
-    return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
+  // 对应 onHide
+  useDidHide(() => { })
+
+  // Taro 对所有小程序页面生命周期都实现了对应的自定义 React Hooks 进行支持
+  // 详情可查阅：【Taro 文档】-> 【进阶指南】->【Hooks】
+  usePullDownRefresh(() => { })
+
+  return (
+    <View className='index'>
+      <View className='container' >
+        <Image style='width:150px; height:150px; border-radius:50%; -webkit-border-radius:50%; -moz-border-radius:50%; box-shadow: 0px 2px 12px 0px rgba(61, 73, 102, 0.2)'
+          src={avatar.pigeon}></Image>
+        <View style='margin-top: 50px; display: flex; flex-direction: column; align-items: center;'>
+          <Text>敬 请 期 待</Text>
+        </View>
       </View>
-    )
-  }
+      <Button onClick={handleClick} style=' position:absolute;right:0px;bottom:0px;width:60px; '>Test</Button>
+
+    </View>
+  )
 }
+
+export default Index
